6129b85ceb4c47654585f635b3d3e297782d8ec6
Yash Deshmukh <yashdeshmukh4925@gmail.com>
2026-07-28 22:45:18 +0530

    Time: 83 ms (66.45%), Space: 74.9 MB (69.59%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr
+ * @param {number} n
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    const result = [];
+
+    function dfs(current, depth) {
+        for (const item of current) {
+            if (Array.isArray(item) && depth < n) {
+                dfs(item, depth + 1);
+            } else {
+                result.push(item);
+            }
+        }
+    }
+
+    dfs(arr, 0);
+    return result;
+};
