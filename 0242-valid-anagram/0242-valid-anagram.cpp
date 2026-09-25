24b48b79154125d78a5efeb3e05d54547763d981
Yash Deshmukh <yashdeshmukh4925@gmail.com>
2026-09-25 22:28:57 +0530

    Time: 7 ms (18.1%), Space: 9.4 MB (99.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!= t.length()){
+        // if(s.length()!= t.length()){
+        //     return false;
+        // }
+        // int freq[26]={0};
+        // for(int i=0;i<s.length();i++){
+        //     freq[s[i]-'a']++;
+        //     freq[t[i]-'a']--;
+
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        if(s.length()!=t.length()){
             return false;
-        }
-        int freq[26]={0};
-        for(int i=0;i<s.length();i++){
-            freq[s[i]-'a']++;
-            freq[t[i]-'a']--;
 
         }
-        for(int i=0;i<26;i++){
-            if(freq[i]!=0){
-                return false;
-            }
-        }
-        return true;
-        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
